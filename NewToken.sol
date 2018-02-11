f1b0000d4388d91e71b0cebe7e2feb02adcfcc90
Namir Bagterian <34695724+Emir999@users.noreply.github.com>
2018-02-11 15:56:39 +0300

    Update NewToken.sol

--- NewToken.sol
+++ NewToken.sol
@@ -4,10 +4,10 @@ import "./ERC20Standard.sol";
 
 contract NewToken is ERC20Standard {
 	function NewToken() {
-		totalSupply = 123;
+		totalSupply = 10000000;
 		name = "Best ever coin";
-		decimals = 4;
-		symbol = "BEC";
+		decimals = 8;
+		symbol = "FAN";
 		version = "1.0";
 		balances[msg.sender] = totalSupply;
 	}
